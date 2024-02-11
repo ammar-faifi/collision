762df702651e9f7c6ab7e837f063a933ff732982
Ammar Alfaifi <me@ammarf.sa>
2024-02-11 06:33:42 +0300

    add gravity, better collision sepration

--- my_app.c
+++ my_app.c
@@ -1,8 +1,10 @@
 #include "raylib.h"
+#include "raymath.h"
 #include <stdlib.h>
+#include <time.h>
 
-#define BALL_NUM 100
-#define BALL_RADIUS 5
+#define BALL_NUM 10
+#define BALL_RADIUS 40
 
 int main(void) {
   const int screenWidth = 800;
@@ -14,20 +16,21 @@ int main(void) {
 
   Vector2 balls_speed[BALL_NUM];
   Vector2 balls_position[BALL_NUM];
+  Vector2 accel = {0.f, 200.0f};
 
-  srand(111);
+  srand(time(NULL));
   for (int i = 0; i < BALL_NUM; i++) {
-    balls_speed[i].x = (float)rand() / RAND_MAX * 50.0f;
-    balls_speed[i].y = (float)rand() / RAND_MAX * 50.0f;
+    balls_speed[i].x = (float)rand() / RAND_MAX * 200.0f;
+    balls_speed[i].y = (float)rand() / RAND_MAX * 200.0f;
 
-    balls_position[i].x = (float)rand() / RAND_MAX * 100 + BALL_RADIUS;
-    balls_position[i].y = (float)rand() / RAND_MAX * 100 + BALL_RADIUS;
+    balls_position[i].x = (float)rand() / RAND_MAX * 800 + BALL_RADIUS;
+    balls_position[i].y = (float)rand() / RAND_MAX * 500 + BALL_RADIUS;
   }
 
   bool pause = 0;
   int framesCounter = 0;
 
-  SetTargetFPS(60);
+  SetTargetFPS(100);
 
   while (!WindowShouldClose()) // Detect window close button or ESC key
   {
@@ -37,6 +40,10 @@ int main(void) {
 
     if (!pause) {
       for (int i = 0; i < BALL_NUM; i++) {
+        // apply acceleration
+        balls_speed[i].x += accel.x * GetFrameTime();
+        balls_speed[i].y += accel.y * GetFrameTime();
+
         balls_position[i].x += balls_speed[i].x * GetFrameTime();
         balls_position[i].y += balls_speed[i].y * GetFrameTime();
 
@@ -47,6 +54,25 @@ int main(void) {
         if ((balls_position[i].y >= (GetScreenHeight() - 10 - BALL_RADIUS)) ||
             (balls_position[i].y <= BALL_RADIUS))
           balls_speed[i].y *= -1.0f;
+        for (int j = i; j < BALL_NUM; j++) {
+          if (CheckCollisionCircles(balls_position[i], BALL_RADIUS,
+                                    balls_position[j], BALL_RADIUS)) {
+            Vector2 tmp = balls_speed[i];
+            balls_speed[i] = balls_speed[j];
+            balls_speed[j] = tmp;
+
+            // seprate the two intersect circles
+            float overlap =
+                2 * BALL_RADIUS -
+                Vector2Distance(balls_position[j], balls_position[i]);
+            Vector2 normal = Vector2Normalize(
+                Vector2Subtract(balls_position[j], balls_position[i]));
+            normal = Vector2Scale(normal, overlap / 2);
+
+            balls_position[i] = Vector2Subtract(balls_position[i], normal);
+            balls_position[j] = Vector2Add(balls_position[j], normal);
+          }
+        }
       }
     } else
       framesCounter++;
