648a7e50e1a18bb860ff8bc3be3aff253d04f679
ammar-faifi <me@ammarf.sa>
2024-02-17 20:43:03 +0300

    improve readability

--- my_app.c
+++ my_app.c
@@ -3,39 +3,36 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 800
 #define BALL_NUM 500
 #define BALL_RADIUS 15
 #define SUBSTEPS 10
 
 int main(void) {
-  const int screenWidth = 800;
-  const int screenHeight = 800;
-
   SetConfigFlags(FLAG_MSAA_4X_HINT);
-  InitWindow(screenWidth, screenHeight,
+  InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT,
              "raylib [shapes] example - bouncing ball");
 
-  Vector2 balls_speed[BALL_NUM];
-  Vector2 balls_position[BALL_NUM];
-  Vector2 accel = {0.f, 500.0f};
+  Vector2 ballsSpeed[BALL_NUM];
+  Vector2 ballsPosition[BALL_NUM];
+  Vector2 accel = {0.0f, 500.0f};
 
   srand(time(NULL));
   for (int i = 0; i < BALL_NUM; i++) {
-    balls_speed[i].x = (float)rand() / RAND_MAX * 200.0f;
-    balls_speed[i].y = (float)rand() / RAND_MAX * 200.0f;
-
-    balls_position[i].x = (float)rand() / RAND_MAX * 800 + BALL_RADIUS;
-    balls_position[i].y = (float)rand() / RAND_MAX * 500 + BALL_RADIUS;
+    ballsSpeed[i] =
+        (Vector2){GetRandomValue(100, 200), GetRandomValue(100, 200)};
+    ballsPosition[i] =
+        (Vector2){GetRandomValue(BALL_RADIUS, SCREEN_WIDTH - BALL_RADIUS),
+                  GetRandomValue(BALL_RADIUS, SCREEN_HEIGHT - BALL_RADIUS)};
   }
 
-  bool pause = 0;
+  bool pause = false;
   int framesCounter = 0;
 
   SetTargetFPS(60);
 
-  while (!WindowShouldClose()) // Detect window close button or ESC key
-  {
-    // Update
+  while (!WindowShouldClose()) {
     if (IsKeyPressed(KEY_SPACE))
       pause = !pause;
 
@@ -44,66 +41,68 @@ int main(void) {
 
       for (int step = 0; step < SUBSTEPS; step++) {
         for (int i = 0; i < BALL_NUM; i++) {
-          // apply acceleration
-          balls_speed[i].x += accel.x * dt;
-          balls_speed[i].y += accel.y * dt;
+          ballsSpeed[i].x += accel.x * dt;
+          ballsSpeed[i].y += accel.y * dt;
 
-          balls_position[i].x += balls_speed[i].x * dt;
-          balls_position[i].y += balls_speed[i].y * dt;
+          ballsPosition[i].x += ballsSpeed[i].x * dt;
+          ballsPosition[i].y += ballsSpeed[i].y * dt;
 
           // Check walls collision for bouncing
-          if (balls_position[i].x >= (GetScreenWidth() - BALL_RADIUS)) {
-            balls_position[i].x = GetScreenWidth() - BALL_RADIUS;
-            balls_speed[i].x *= -0.5f;
+          if (ballsPosition[i].x >= (GetScreenWidth() - BALL_RADIUS)) {
+            ballsPosition[i].x = GetScreenWidth() - BALL_RADIUS;
+            ballsSpeed[i].x *= -0.5f;
           }
-          if (balls_position[i].x <= BALL_RADIUS) {
-            balls_position[i].x = BALL_RADIUS;
-            balls_speed[i].x *= -0.5f;
+          if (ballsPosition[i].x <= BALL_RADIUS) {
+            ballsPosition[i].x = BALL_RADIUS;
+            ballsSpeed[i].x *= -0.5f;
           }
 
-          if (balls_position[i].y >= (GetScreenHeight() - BALL_RADIUS)) {
-            balls_position[i].y = GetScreenHeight() - BALL_RADIUS;
-            balls_speed[i].y *= -0.5f;
+          if (ballsPosition[i].y >= (GetScreenHeight() - BALL_RADIUS)) {
+            ballsPosition[i].y = GetScreenHeight() - BALL_RADIUS;
+            ballsSpeed[i].y *= -0.5f;
           }
-          if (balls_position[i].y <= BALL_RADIUS) {
-            balls_position[i].y = BALL_RADIUS;
-            balls_speed[i].y *= -0.5f;
+          if (ballsPosition[i].y <= BALL_RADIUS) {
+            ballsPosition[i].y = BALL_RADIUS;
+            ballsSpeed[i].y *= -0.5f;
           }
 
           for (int j = i; j < BALL_NUM; j++) {
-            if (CheckCollisionCircles(balls_position[i], BALL_RADIUS,
-                                      balls_position[j], BALL_RADIUS)) {
-              Vector2 tmp = balls_speed[i];
-              balls_speed[i] = balls_speed[j];
-              balls_speed[j] = tmp;
+            if (i != j &&
+                CheckCollisionCircles(ballsPosition[i], BALL_RADIUS,
+                                      ballsPosition[j], BALL_RADIUS)) {
+              Vector2 tmp = ballsSpeed[i];
+              ballsSpeed[i] = ballsSpeed[j];
+              ballsSpeed[j] = tmp;
 
-              // seprate the two intersect circles
+              // Separate the two intersecting circles
               Vector2 normal = Vector2Normalize(
-                  Vector2Subtract(balls_position[j], balls_position[i]));
+                  Vector2Subtract(ballsPosition[j], ballsPosition[i]));
               float overlap =
                   2 * BALL_RADIUS -
-                  Vector2Distance(balls_position[j], balls_position[i]);
+                  Vector2Distance(ballsPosition[j], ballsPosition[i]);
               normal = Vector2Scale(normal, overlap / 2);
 
-              balls_position[i] = Vector2Subtract(balls_position[i], normal);
-              balls_position[j] = Vector2Add(balls_position[j], normal);
+              ballsPosition[i] = Vector2Subtract(ballsPosition[i], normal);
+              ballsPosition[j] = Vector2Add(ballsPosition[j], normal);
             }
           }
         }
       }
-    } else
+    } else {
       framesCounter++;
-
-    // Draw
-    BeginDrawing();
-    ClearBackground(RAYWHITE);
-    for (int i = 0; i < BALL_NUM; i++) {
-      DrawCircleV(balls_position[i], (float)BALL_RADIUS, BLACK);
     }
 
-    // On pause, we draw a blinking message
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+
+    for (int i = 0; i < BALL_NUM; i++) {
+      DrawCircleV(ballsPosition[i], (float)BALL_RADIUS, BLACK);
+    }
+
+    // On pause, draw a blinking message
     if (pause && ((framesCounter / 30) % 2))
-      DrawText("PAUSED", 350, 200, 30, GRAY);
+      DrawText("PAUSED", SCREEN_WIDTH / 2 - MeasureText("PAUSED", 30) / 2,
+               SCREEN_HEIGHT / 2 - 15, 30, GRAY);
 
     DrawFPS(10, 10);
     EndDrawing();
