2a1ca3bf7b4240c4eade08fb8f5693090898d6ef
ammar-faifi <me@ammarf.sa>
2024-02-17 20:17:34 +0300

    use sub-steps tech, more control on constraint

--- my_app.c
+++ my_app.c
@@ -3,12 +3,13 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define BALL_NUM 10
-#define BALL_RADIUS 40
+#define BALL_NUM 500
+#define BALL_RADIUS 15
+#define SUBSTEPS 10
 
 int main(void) {
   const int screenWidth = 800;
-  const int screenHeight = 450;
+  const int screenHeight = 800;
 
   SetConfigFlags(FLAG_MSAA_4X_HINT);
   InitWindow(screenWidth, screenHeight,
@@ -16,7 +17,7 @@ int main(void) {
 
   Vector2 balls_speed[BALL_NUM];
   Vector2 balls_position[BALL_NUM];
-  Vector2 accel = {0.f, 200.0f};
+  Vector2 accel = {0.f, 500.0f};
 
   srand(time(NULL));
   for (int i = 0; i < BALL_NUM; i++) {
@@ -30,7 +31,7 @@ int main(void) {
   bool pause = 0;
   int framesCounter = 0;
 
-  SetTargetFPS(100);
+  SetTargetFPS(60);
 
   while (!WindowShouldClose()) // Detect window close button or ESC key
   {
@@ -39,38 +40,54 @@ int main(void) {
       pause = !pause;
 
     if (!pause) {
-      for (int i = 0; i < BALL_NUM; i++) {
-        // apply acceleration
-        balls_speed[i].x += accel.x * GetFrameTime();
-        balls_speed[i].y += accel.y * GetFrameTime();
+      float dt = GetFrameTime() / SUBSTEPS;
 
-        balls_position[i].x += balls_speed[i].x * GetFrameTime();
-        balls_position[i].y += balls_speed[i].y * GetFrameTime();
+      for (int step = 0; step < SUBSTEPS; step++) {
+        for (int i = 0; i < BALL_NUM; i++) {
+          // apply acceleration
+          balls_speed[i].x += accel.x * dt;
+          balls_speed[i].y += accel.y * dt;
 
-        // Check walls collision for bouncing
-        if ((balls_position[i].x >= (GetScreenWidth() - 10 - BALL_RADIUS)) ||
-            (balls_position[i].x <= BALL_RADIUS))
-          balls_speed[i].x *= -1.0f;
-        if ((balls_position[i].y >= (GetScreenHeight() - 10 - BALL_RADIUS)) ||
-            (balls_position[i].y <= BALL_RADIUS))
-          balls_speed[i].y *= -1.0f;
-        for (int j = i; j < BALL_NUM; j++) {
-          if (CheckCollisionCircles(balls_position[i], BALL_RADIUS,
-                                    balls_position[j], BALL_RADIUS)) {
-            Vector2 tmp = balls_speed[i];
-            balls_speed[i] = balls_speed[j];
-            balls_speed[j] = tmp;
+          balls_position[i].x += balls_speed[i].x * dt;
+          balls_position[i].y += balls_speed[i].y * dt;
 
-            // seprate the two intersect circles
-            float overlap =
-                2 * BALL_RADIUS -
-                Vector2Distance(balls_position[j], balls_position[i]);
-            Vector2 normal = Vector2Normalize(
-                Vector2Subtract(balls_position[j], balls_position[i]));
-            normal = Vector2Scale(normal, overlap / 2);
+          // Check walls collision for bouncing
+          if (balls_position[i].x >= (GetScreenWidth() - BALL_RADIUS)) {
+            balls_position[i].x = GetScreenWidth() - BALL_RADIUS;
+            balls_speed[i].x *= -0.5f;
+          }
+          if (balls_position[i].x <= BALL_RADIUS) {
+            balls_position[i].x = BALL_RADIUS;
+            balls_speed[i].x *= -0.5f;
+          }
 
-            balls_position[i] = Vector2Subtract(balls_position[i], normal);
-            balls_position[j] = Vector2Add(balls_position[j], normal);
+          if (balls_position[i].y >= (GetScreenHeight() - BALL_RADIUS)) {
+            balls_position[i].y = GetScreenHeight() - BALL_RADIUS;
+            balls_speed[i].y *= -0.5f;
+          }
+          if (balls_position[i].y <= BALL_RADIUS) {
+            balls_position[i].y = BALL_RADIUS;
+            balls_speed[i].y *= -0.5f;
+          }
+
+          for (int j = i; j < BALL_NUM; j++) {
+            if (CheckCollisionCircles(balls_position[i], BALL_RADIUS,
+                                      balls_position[j], BALL_RADIUS)) {
+              Vector2 tmp = balls_speed[i];
+              balls_speed[i] = balls_speed[j];
+              balls_speed[j] = tmp;
+
+              // seprate the two intersect circles
+              Vector2 normal = Vector2Normalize(
+                  Vector2Subtract(balls_position[j], balls_position[i]));
+              float overlap =
+                  2 * BALL_RADIUS -
+                  Vector2Distance(balls_position[j], balls_position[i]);
+              normal = Vector2Scale(normal, overlap / 2);
+
+              balls_position[i] = Vector2Subtract(balls_position[i], normal);
+              balls_position[j] = Vector2Add(balls_position[j], normal);
+            }
           }
         }
       }
@@ -81,7 +98,7 @@ int main(void) {
     BeginDrawing();
     ClearBackground(RAYWHITE);
     for (int i = 0; i < BALL_NUM; i++) {
-      DrawCircleV(balls_position[i], (float)BALL_RADIUS, MAROON);
+      DrawCircleV(balls_position[i], (float)BALL_RADIUS, BLACK);
     }
 
     // On pause, we draw a blinking message
