e8f2ef0d2cb5c85d56aa79c47ebfc180d13ca2c3
ammar-faifi <me@ammarf.sa>
2025-01-24 20:59:00 +0300

    A new version

--- main.c
+++ main.c
@@ -1,110 +1,102 @@
-#include "raylib.h"
-#include "raymath.h"
+#include <stdio.h>
 #include <stdlib.h>
 
-#define SCREEN_WIDTH 800
-#define SCREEN_HEIGHT 800
-#define BALL_NUM 90
-#define BALL_RADIUS 20
-#define SUBSTEPS 4
+#include "raylib.h"
+#include "raymath.h"
 
-int main(void) {
+#define SCREEN_WIDTH 1080 / 2
+#define SCREEN_HEIGHT 1920 / 2
+#define CIRC_RADIUS 8
+#define CIRC_NUM 2000
+#define SUB_STEPS 30
+
+int draw_limit;
+int frames_counter;
+
+int main() {
   SetConfigFlags(FLAG_MSAA_4X_HINT);
-  InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "raylib - particles collision");
-
-  Vector2 gravity = {0.0f, 500.0f};
-  Vector2 ballsPos[BALL_NUM];
-  Vector2 ballsVel[BALL_NUM];
-  Vector2 ballsAcc[BALL_NUM] = {gravity.x, gravity.y};
-  Color ballsColor[BALL_NUM];
-
-  for (int i = 0; i < BALL_NUM; i++) {
-    ballsVel[i] = (Vector2){GetRandomValue(100, 200), GetRandomValue(10, 100)};
-    ballsPos[i] =
-        (Vector2){GetRandomValue(BALL_RADIUS, SCREEN_WIDTH - BALL_RADIUS),
-                  GetRandomValue(BALL_RADIUS, SCREEN_HEIGHT - BALL_RADIUS)};
-    ballsColor[i] = ColorFromHSV(GetRandomValue(0, 360), 0.5f, 0.7f);
-  }
-
-  bool pause = false;
-  int framesCounter = 0;
-
+  InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "My collision sim");
   SetTargetFPS(60);
 
+  Vector2 gravity = {0.f, 1000.f};
+  Vector2 circPos[CIRC_NUM];
+  Vector2 circVel[CIRC_NUM];
+  Vector2 circAcc[CIRC_NUM] = {{gravity.x, gravity.y}};
+  Color circColor[CIRC_NUM];
+
+  for (int i = 0; i < CIRC_NUM; i++) {
+    circPos[i] = (Vector2){GetRandomValue(4 * CIRC_RADIUS, 10 * CIRC_RADIUS),
+                           CIRC_RADIUS * 10};
+    circVel[i] = (Vector2){GetRandomValue(200, 300), GetRandomValue(1, 100)};
+    circColor[i] = ColorFromHSV(GetRandomValue(0, 360), 0.5f, 0.7f);
+  }
+
   while (!WindowShouldClose()) {
-    if (IsKeyPressed(KEY_SPACE))
-      pause = !pause;
 
-    if (!pause) {
-      float dt = GetFrameTime() / SUBSTEPS;
+    if (draw_limit < CIRC_NUM && frames_counter % 4)
+      draw_limit++;
 
-      for (int step = 0; step < SUBSTEPS; step++) {
-        for (int i = 0; i < BALL_NUM; i++) {
-          // Update position
-          ballsPos[i].x += ballsVel[i].x * dt + ballsAcc[i].x * dt * dt * 0.5f;
-          ballsPos[i].y += ballsVel[i].y * dt + ballsAcc[i].y * dt * dt * 0.5f;
+    float dt = GetFrameTime() / SUB_STEPS;
 
-          // Update velocity
-          ballsVel[i].x += gravity.x * dt;
-          ballsVel[i].y += gravity.y * dt;
+    for (int step = 0; step < SUB_STEPS; step++) {
+      for (int i = 0; i < draw_limit; i++) {
+        // Update Positions
+        circPos[i].x += circVel[i].x * dt + circAcc[i].x * dt * dt * 0.5f;
+        circPos[i].y += circVel[i].y * dt + circAcc[i].y * dt * dt * 0.5f;
 
-          // Check walls collision for bouncing
-          if (ballsPos[i].x >= (GetScreenWidth() - BALL_RADIUS)) {
-            ballsPos[i].x = GetScreenWidth() - BALL_RADIUS;
-            ballsVel[i].x = 0.0f;
-          }
-          if (ballsPos[i].x <= BALL_RADIUS) {
-            ballsPos[i].x = BALL_RADIUS;
-            ballsVel[i].x = 0.0f;
-          }
+        // Update Vellocities
+        circVel[i].x += gravity.x * dt;
+        circVel[i].y += gravity.y * dt;
 
-          if (ballsPos[i].y >= (GetScreenHeight() - BALL_RADIUS)) {
-            ballsPos[i].y = GetScreenHeight() - BALL_RADIUS;
-            ballsVel[i].y = 0.0f;
-          }
-          if (ballsPos[i].y <= BALL_RADIUS) {
-            ballsPos[i].y = BALL_RADIUS;
-            ballsVel[i].y = 0.0f;
-          }
+        // Check walls collision for bouncing
+        if (circPos[i].x >= GetScreenWidth() - CIRC_RADIUS) {
+          circPos[i].x = GetScreenWidth() - CIRC_RADIUS;
+          circVel[i].x = circVel[i].x * 0.6f;
+        } else if (circPos[i].x < CIRC_RADIUS) {
+          circPos[i].x = CIRC_RADIUS;
+          circVel[i].x = 0.f;
+        }
 
-          for (int j = i; j < BALL_NUM; j++) {
-            if (i != j && CheckCollisionCircles(ballsPos[i], BALL_RADIUS,
-                                                ballsPos[j], BALL_RADIUS)) {
-              Vector2 tmp = ballsVel[i];
-              ballsVel[i] = ballsVel[j];
-              ballsVel[j] = tmp;
+        if (circPos[i].y >= GetScreenHeight() - CIRC_RADIUS) {
+          circPos[i].y = GetScreenHeight() - CIRC_RADIUS;
+          circVel[i].y = -0.6f * circVel[i].y;
+        } else if (circPos[i].y < CIRC_RADIUS) {
+          circPos[i].y = CIRC_RADIUS;
+          circVel[i].y = 0.f;
+        }
 
-              // Separate the two intersecting circles
-              Vector2 normal =
-                  Vector2Normalize(Vector2Subtract(ballsPos[j], ballsPos[i]));
-              float overlap =
-                  2.f * BALL_RADIUS - Vector2Distance(ballsPos[j], ballsPos[i]);
-              normal = Vector2Scale(normal, overlap / 2.f);
+        // Check and solve collision
+        for (int j = i; j < draw_limit; j++) {
+          if (i != j && CheckCollisionCircles(circPos[i], CIRC_RADIUS,
+                                              circPos[j], CIRC_RADIUS)) {
+            Vector2 tmp = circVel[i];
+            circVel[i] = circVel[j];
+            circVel[j] = tmp;
 
-              ballsPos[i] = Vector2Subtract(ballsPos[i], normal);
-              ballsPos[j] = Vector2Add(ballsPos[j], normal);
-            }
+            // Separate the two intersecting circles
+            Vector2 normal =
+                Vector2Normalize(Vector2Subtract(circPos[j], circPos[i]));
+            float overlap =
+                2.f * CIRC_RADIUS - Vector2Distance(circPos[j], circPos[i]);
+            normal = Vector2Scale(normal, overlap / 2.f);
+
+            circPos[i] = Vector2Subtract(circPos[i], normal);
+            circPos[j] = Vector2Add(circPos[j], normal);
           }
         }
       }
-    } else {
-      framesCounter++;
     }
 
     BeginDrawing();
     ClearBackground(BLACK);
 
-    for (int i = 0; i < BALL_NUM; i++) {
-      DrawCircleV(ballsPos[i], (float)BALL_RADIUS, ballsColor[i]);
+    for (int i = 0; i < draw_limit; i++) {
+      DrawCircleV(circPos[i], CIRC_RADIUS, circColor[i]);
     }
-
-    // On pause, draw a blinking message
-    if (pause && ((framesCounter / 30) % 2))
-      DrawText("PAUSED", SCREEN_WIDTH / 2 - MeasureText("PAUSED", 30) / 2,
-               SCREEN_HEIGHT / 2 - 15, 30, GRAY);
 
     DrawFPS(10, 10);
     EndDrawing();
+    frames_counter++;
   }
 
   CloseWindow();
